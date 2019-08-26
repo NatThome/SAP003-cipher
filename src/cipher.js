6e8d45ba1459c23f4de08503d8d25483f6d3ff24
NatThome <nathythome05@gmail.com>
2019-08-26 16:06:28 -0300

    funcao cod e decod minuscula

--- src/cipher.js
+++ src/cipher.js
@@ -3,24 +3,42 @@ window.cipher = {
   decode: decode
 };
 
-function encode(textEncod, offset){
+function encode(offset, textEncod) {
   let resultado = "";
-  for (let i = 0; i < textEncod.length; i ++){
-    //if(textEncod.charCodeAt(i) === 32){  
-    //}
-    encodedMessage = ((textEncod.charCodeAt(i) - 65 + offset) % 26) + 65;
-    resultado += String.fromCharCode(encodedMessage);
+  
+  for (let i = 0; i < textEncod.length; i ++) {
+    if (textEncod.charCodeAt(i)>= 65 && textEncod.charCodeAt(i)<=90) {
+      let encodedMessage = ((textEncod.charCodeAt(i) - 65 + offset) % 26) + 65;
+      resultado += String.fromCharCode(encodedMessage);
     }
-    return resultado;
-}
-
-function decode(textDecod, offset){
-  let resultado = "";
-  for (let i = 0; i < textDecod.length; i ++){
-    decodedMessage = ((textDecod.charCodeAt(i) - 90 - offset) % 26) + 90;
-    resultado += String.fromCharCode(decodedMessage);
+    else if (textEncod.charCodeAt(i)>= 97 && textEncod.charCodeAt(i)<=122) {
+      let encodedMessageLow = ((textEncod.charCodeAt(i) - 97 + offset) % 26) + 97;
+      resultado += String.fromCharCode(encodedMessageLow);
+    }
+    else {
+      let encodedMessageEsp = textEncod.charCodeAt(i);
+      resultado += String.fromCharCode(encodedMessageEsp);
+    }
   }
   return resultado;
 }
 
+function decode(offset, textDecod) {
+  let resultado = "";
 
+  for (let i = 0; i < textDecod.length; i ++) {
+    if (textDecod.charCodeAt(i)>= 65 && textDecod.charCodeAt(i)<=90) {
+      let decodedMessage = ((textDecod.charCodeAt(i) - 90 - offset) % 26) + 90;
+      resultado += String.fromCharCode(decodedMessage);
+    }
+    else if (textDecod.charCodeAt(i)>= 97 && textDecod.charCodeAt(i)<=122) {
+      let encodedMessageLow = ((textDecod.charCodeAt(i) - 122 - offset) % 26) + 122;
+      resultado += String.fromCharCode(encodedMessageLow);
+    }
+    else {
+      let encodedMessageEsp = textDecod.charCodeAt(i);
+      resultado += String.fromCharCode(encodedMessageEsp);
+    }
+  }
+  return resultado;
+}
--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-function cifra(){
-    const textEncod = document.getElementById("cifra_texto").value;
-    let offset = parseInt(document.getElementById("chave").value);
-    let resultEncode = window.cipher.encode(textEncod, offset);
-    document.getElementById("cifrado").innerHTML = `${resultEncode}`;
+document.getElementById("btn-cifra").addEventListener("click", cifra);
+function cifra() {
+  const textEncod = document.getElementById("cifra_texto").value;
+  let offset = parseInt(document.getElementById("chave").value);
+  let resultEncode = window.cipher.encode(offset, textEncod);
+  document.getElementById("cifrado").innerHTML = `${resultEncode}`;
 }
 
-function decifra(){
-    const textDecod = document.getElementById("decifra_texto").value;
-    let offset = parseInt(document.getElementById("chave").value);
-    let resultDecode = window.cipher.decode(textDecod, offset);
-    document.getElementById("cifrado").innerHTML = `${resultDecode}`;
+document.getElementById("btn-decifra").addEventListener("click", decifra);
+function decifra() {
+  const textDecod = document.getElementById("decifra_texto").value;
+  let offset = parseInt(document.getElementById("chave").value);
+  let resultDecode = window.cipher.decode(offset, textDecod);
+  document.getElementById("cifrado").innerHTML = `${resultDecode}`;
 }
-
- 
